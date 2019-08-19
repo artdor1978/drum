5596e1bde66cacfd9c3a71ef20ef55e0693e06cc
Artem Dorokhov <artdor1978@gmail.com>
2019-08-19 13:40:06 +0300

    Updates

---
+++ precache-manifest.aa77c68e7648b898a9a57d67bc522197.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b2f55d22917fe806a345797d8035a2",
+    "url": "/drum/index.html"
+  },
+  {
+    "revision": "d42554bd7a1e3855f4a5",
+    "url": "/drum/static/css/main.1ae2147f.chunk.css"
+  },
+  {
+    "revision": "f2ef5652ce0722998c02",
+    "url": "/drum/static/js/2.67474d72.chunk.js"
+  },
+  {
+    "revision": "d42554bd7a1e3855f4a5",
+    "url": "/drum/static/js/main.477695c1.chunk.js"
+  },
+  {
+    "revision": "be722fef866151f9d9b0",
+    "url": "/drum/static/js/runtime~main.65d06daa.js"
+  }
+]);
